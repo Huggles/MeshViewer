7a795e7d51a07826bded679bd81a5859f14c89c4
Dan Nicholson <dan.nicholson@appsolutely.nl>
2019-06-04 12:00:19 +0100

    Add messaging

--- force-app/main/default/lwc/balanceCard/balanceCard.js
+++ force-app/main/default/lwc/balanceCard/balanceCard.js
@@ -4,17 +4,21 @@ import addBudged from '@salesforce/apex/ConfigAppController.addBudged';
 import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import Config_Balance_Threshold_Description from '@salesforce/label/c.Config_Balance_Threshold_Description';
+import Config_Balance_Updated from '@salesforce/label/c.Config_Balance_Updated';
 
 export default class balanceCard extends LightningElement {
 
     @track ready = false;
     @track balanceAmount;
+    @track error;
+    @track success;
     amountToAdd = 10;
 
     wiredBalanceResult;
 
     label = {
-        Config_Balance_Threshold_Description
+        Config_Balance_Threshold_Description,
+        Config_Balance_Updated
     }
     
     @wire(getUserBalance)
@@ -38,32 +42,25 @@ export default class balanceCard extends LightningElement {
 
     addBudged(event){
         addBudged({Amount: this.amountToAdd}).then(result => {
-            console.log(result);
-            console.log('SUCCESS123');
-            const evt = new ShowToastEvent({
-                title: 'Success',
-                message: 'The balance was changed',
-                variant: 'success'
-            });
-            this.dispatchEvent(evt);
-            const event = new CustomEvent('userOnboarded', { // why here?
-                // detail contains only primitives
-                //detail: this.product.fields.Id.value
-            });
-            // Fire the event from c-tile
-            this.dispatchEvent(event);
-            //return null;
+            // @todo these don't work in VF apparently
+            // const evt = new ShowToastEvent({
+            //     title: 'Success',
+            //     message: 'The balance was changed',
+            //     variant: 'success'
+            // });
+            // this.dispatchEvent(evt);
+            this.success = this.label.Config_Balance_Updated;
             return refreshApex(this.wiredBalanceResult);
         })
         .catch(error => {
             this.error = error;
-            console.log(this.error);
-            const evt = new ShowToastEvent({
-                title: 'Error',
-                message: 'There was an error during org activation.',
-                variant: 'error'
-            });
-            this.dispatchEvent(evt);
+            // @todo these don't work in VF apparently
+            // const evt = new ShowToastEvent({
+            //     title: 'Error',
+            //     message: 'There was an error during org activation.',
+            //     variant: 'error'
+            // });
+            // this.dispatchEvent(evt);
         });
     }
 
