a4f732dbc16d826949b6ba2392a10b449a18cd35
tejaswini <tejaswini.dandi@appsolutely.nl>
2019-06-05 10:04:23 +0200

    added custom labels

--- force-app/main/default/lwc/balanceCard/balanceCard.js
+++ force-app/main/default/lwc/balanceCard/balanceCard.js
@@ -3,14 +3,31 @@ import getUserBalance from '@salesforce/apex/ConfigAppController.getUserBalance'
 import addBudged from '@salesforce/apex/ConfigAppController.addBudged';
 import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-
-
+import Config_Balance_Management_Text from '@salesforce/label/c.Config_Balance_Management_Text';
+import Config_Current_Budget from '@salesforce/label/c.Config_Current_Budget';
+import Config_Loading from '@salesforce/label/c.Config_Loading';
+import Config_Balance_Management from '@salesforce/label/c.Config_Balance_Management';
+import Config_Current_Threshold from '@salesforce/label/c.Config_Current_Threshold';
+import Config_Add_Budget from '@salesforce/label/c.Config_Add_Budget';
+import Config_Balance_Changed from '@salesforce/label/c.Config_Balance_Changed';
+import Config_Balance_Error from '@salesforce/label/c.Config_Balance_Error';
 
 export default class balanceCard extends LightningElement {
 
     @track ready = false;
     @track balanceAmount;
     amountToAdd = 10;
+
+    label = {
+        Config_Balance_Management_Text,
+        Config_Current_Budget,
+        Config_Balance_Management,
+        Config_Loading,
+        Config_Current_Threshold,
+        Config_Add_Budget,
+        Config_Balance_Changed,
+        Config_Balance_Error
+    }
 
     wiredBalanceResult;
     
@@ -39,7 +56,7 @@ export default class balanceCard extends LightningElement {
             console.log('SUCCESS123');
             const evt = new ShowToastEvent({
                 title: 'Success',
-                message: 'The balance was changed',
+                message:  Config_Balance_Changed,
                 variant: 'success'
             });
             this.dispatchEvent(evt);
@@ -57,7 +74,7 @@ export default class balanceCard extends LightningElement {
             console.log(this.error);
             const evt = new ShowToastEvent({
                 title: 'Error',
-                message: 'There was an error during org activation.',
+                message: Config_Balance_Error,
                 variant: 'error'
             });
             this.dispatchEvent(evt);
