5dff0f9e928a43768476714237822e1f8d718287
Jaap Branderhorst <jaap.branderhorst@appsolutely.nl>
2019-07-07 15:34:48 +0200

    Manual tested the CreditSafe get company. Works. Still need to write unit tests. Need to remove VAT number button and add related lists for addresses.

--- force-app/main/default/aura/dossierDetails/dossierDetailsController.js
+++ force-app/main/default/aura/dossierDetails/dossierDetailsController.js
@@ -21,8 +21,9 @@
     onSelect : function(component, event, helper) {
         var params = event.getParams();
         // TODO: check why the account Id is missing from the params
-        var callParams = {dossierNumber: params.DossierNumber, establishmentNumber: params.EstablishmentNumber, selectedDataVendor: params.selectedDataVendor, creditSafeId: params.creditSafeNumber};
+        var callParams = {dossierNumber: params.DossierNumber, establishmentNumber: params.EstablishmentNumber, selectedDataVendor: params.selectedDataVendor, creditSafeId: params.creditSafeId};
         // TODO: does this work with CreditSafe?
+        // and is it necessary even?
         component.set('v.selected', params.DossierNumber);
         helper.callServer(component, 'c.createDossier', callParams, function(response) {
             helper.handleCompanyData(component, response);
--- force-app/main/default/aura/dossierSearchResults/dossierSearchResultsController.js
+++ force-app/main/default/aura/dossierSearchResults/dossierSearchResultsController.js
@@ -7,14 +7,13 @@
      */
     onSelect : function(component, event, helper) {
         var params = event.getParams();
-        console.log(JSON.stringify(params));
         component.set('v.selected', params.DossierNumber);
         component.set('v.selectedEstablishment', params.EstablishmentNumber);
         component.set('v.selectedDataVendor', params.selectedDataVendor);
         component.set('v.selectedCreditSafeId', params.creditSafeId);
         var results = component.find('searchResult');
         for (var i = 0; i < results.length; i++) {
-            if (results[i].get('v.result').dossier_number !== params.DossierNumber || results[i].get('v.result').establishment_number !== params.EstablishmentNumber) {
+            if ((results[i].get('v.result').dossier_number !== params.DossierNumber && results[i].get('v.result').establishment_number !== params.EstablishmentNumber) || results[i].get('v.result').creditSafeId !== params.creditSafeId) {
                 results[i].set('v.icon', 'action:record');
             }
         }
@@ -29,7 +28,7 @@
         // Getting the event
         var updateEvent = component.getEvent("dossierSelectionConfirmedEvent");
         // Setting the param on the event 
-        updateEvent.setParams({ "DossierNumber": component.get('v.selected'), "EstablishmentNumber": component.get('v.selectedEstablishment'), "selectedDataVendor": component.get('v.selectedDataVendor') , "creditSafeNumber": component.get('v.selectedCreditSafeId') });
+        updateEvent.setParams({ "DossierNumber": component.get('v.selected'), "EstablishmentNumber": component.get('v.selectedEstablishment'), "selectedDataVendor": component.get('v.selectedDataVendor') , "creditSafeId": component.get('v.selectedCreditSafeId') });
         // Fire the event so all the components can hear it
         updateEvent.fire();
     }
