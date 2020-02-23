745f6c22fa8104a0d734114078670e988c3b2e7a
jaapbranderhorst <jaap.branderhorst@appsolutely.nl>
2020-02-23 11:54:32 +0100

    Added a custom toast lwc customToast.html. The standard ShowToast component doesn't work in Flow. Added a flowErrorHandler.html. In the end this lwc component should be able to show flow errors. Still needs work.

---
+++ force-app/main/default/lwc/flowErrorHandler/flowErrorHandler.js
@@ -0,0 +1,35 @@
+/**
+ * Created by jaapbranderhorst on 23/02/2020.
+ */
+
+import {LightningElement, api} from 'lwc';
+
+export default class FlowErrorHandler extends LightningElement {
+
+    @api title = 'Sample Title';
+    @api message = 'Sample Message';
+    // @api variant = 'error';
+    // @api autoCloseTime = 5000;
+    // @api autoClose = false;
+    // @api autoCloseErrorWarning = false;
+
+    toastRendered = false;
+
+    renderedCallback() {
+        if (!this.toastRendered) {
+            this.template.querySelector('c-custom-toast').showToast();
+            this.toastRendered = true;
+        }
+    }
+
+    // showToast() {
+    //     const event = new ShowToastEvent({
+    //         title: this.title,
+    //         message: this.errorMessage
+    //     });
+    //     this.dispatchEvent(event);
+    // }
+
+
+
+}
