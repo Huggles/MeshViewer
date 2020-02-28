ebf783bb1f1f6b56d07de97b475839d608f3e0e6
jaapbranderhorst <jaap.branderhorst@appsolutely.nl>
2020-02-28 22:49:00 +0100

    Added a globalFlowQuickAction.cmp as an 'abstract' component so it's easy to create new aura global quick actions that use Flow

--- force-app/main/default/aura/globalFlowQuickAction/globalFlowQuickActionController.js
+++ force-app/main/default/aura/globalFlowQuickAction/globalFlowQuickActionController.js
@@ -1,22 +1,26 @@
+/**
+ * Created by jaapbranderhorst on 28/02/2020.
+ */
+
 ({
     doInit : function(component, event, helper) {
         $A.createComponent("c:flowContainer",
             {"flowName": "Organisation_Search_Flow",
-            "flowChangeEvent" : component.getReference("c.handleFlowChangeEvent")},
-           function(content, status) {
-               if (status === "SUCCESS") {
-                   var promiseModal = component.find('overlayLib').showCustomModal({
-                       body: content,
-                       showCloseButton: true,
-                       closeCallback: function() {
+                "flowChangeEvent" : component.getReference("c.handleFlowChangeEvent")},
+            function(content, status) {
+                if (status === "SUCCESS") {
+                    var promiseModal = component.find('overlayLib').showCustomModal({
+                        body: content,
+                        showCloseButton: true,
+                        closeCallback: function() {
                             var dismissActionPanel = $A.get("e.force:closeQuickAction");
                             dismissActionPanel.fire();
-                       }
-                   });
-                   component.set("v.overlayPromise", promiseModal);
+                        }
+                    });
+                    component.set("v.overlayPromise", promiseModal);
 
-               }
-           }
+                }
+            }
         );
     },
     handleFlowChangeEvent : function(component, event, helper) {
