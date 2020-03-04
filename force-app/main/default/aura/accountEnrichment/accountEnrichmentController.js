dd117ec03ce47981e80644df688b185f0757b8e1
jaapbranderhorst <jaap.branderhorst@appsolutely.nl>
2020-03-04 23:03:42 +0100

    Implemented the VAT functionality completely. Get VAT number on accountEnrichment.cmp now works and only shows up on the right moments. Only tested for Dutch dossiers as of now.

--- force-app/main/default/aura/accountEnrichment/accountEnrichmentController.js
+++ force-app/main/default/aura/accountEnrichment/accountEnrichmentController.js
@@ -26,7 +26,7 @@
         var eventParams = event.getParams();
         if(eventParams.changeType === "LOADED") {
             var dossier = component.get("v.dossier");
-            if (!dossier.appsolutely__VAT_Number__c) {
+            if (!dossier.appsolutely__VAT_Number__c && !dossier.appsolutely__No_VAT__c) {
                 component.set("v.showVAT", true);
             } else {
                 component.set("v.showVAT", false);
@@ -38,5 +38,26 @@
         if (error && error != '') {
             helper.showToast(component, $A.get('$Label.c.Error'), error);
         }
+    },
+    getVAT : function(component, event, helper) {
+        var callParams = {dossierId: component.get('v.accountRecord').appsolutely__Business_Dossier__c};
+        helper.callServer(component, 'c.updateDossierWithVAT', callParams, function(response) {
+            if (response && (response.appsolutely__VAT_Number__c !== undefined || response.appsolutely__No_VAT_Number__c)) {
+                if (response.appsolutely__VAT_Number__c !== undefined) {
+                    helper.showToast(component, $A.get('$Label.c.Success'), $A.get('$Label.c.Dossier_Account_Update_Completed'), 'success');
+                } else {
+                    helper.showToast(component, $A.get('$Label.c.Success'), $A.get('$Label.c.VAT_Not_Found'));
+                }
+            }
+            else {
+                var errors = response.getError();
+                if (errors && errors[0] && errors[0].message) {
+                    helper.showToast(component, $A.get('$Label.c.Error'), errors[0].message, 'error');
+                } else {
+                    helper.showToast(component, $A.get('$Label.c.Error'), $A.get('$Label.c.Error_Unknown'), 'error');
+                }
+            }
+            component.find('dossierLoader').reloadRecord(true);
+        });
     }
 });
--- force-app/main/default/aura/accountEnrichment/accountEnrichmentHelper.js
+++ force-app/main/default/aura/accountEnrichment/accountEnrichmentHelper.js
@@ -2,22 +2,74 @@
  * Created by jaapbranderhorst on 29/02/2020.
  */
 
-({/**
+({  /**
+     * Call Apex Method
+     * @param {*} component
+     * @param {string} methodName The name of the apex method to call
+     * @param {object} params An object containing the arguments to pass to the apex method
+     * @param {function} callback A js function to be called on successful response from apex
+     * @param {*} stubResponse To be removed, response to stub calls (to avoid costly API calls).
+     */
+    callServer : function(component, methodName, params, callback, stubResponse) {
+
+        // @todo remove after development
+        if (stubResponse) {
+            callback(stubResponse);
+            return;
+        }
+
+        // create a one-time use instance of the serverEcho action
+        // in the server-side controller
+        var _this = this;
+        var action = component.get(methodName);
+        action.setParams(params);
+
+        // Create a callback that is executed after
+        // the server-side action returns
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                if(response.getReturnValue().state ==="SUCCESS"){
+                    callback(response.getReturnValue().response);
+                }else{
+                    _this.showToast(component, response.getReturnValue().errorMsg);
+                }
+            }
+            else if (state === "INCOMPLETE") {
+                _this.showToast(component, $A.get('$Label.c.Error'), $A.get('$Label.c.Error_Incomplete'), 'error');
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors && errors[0] && errors[0].message) {
+                    _this.showToast(component, errors[0].message);
+                } else {
+                    _this.showToast(component, $A.get('$Label.c.Error'), $A.get('$Label.c.Error_Unknown'), 'error');
+                }
+            }
+        });
+
+        // optionally set storable, abortable, background flag here
+
+        // A client-side action could cause multiple events,
+        // which could trigger other events and
+        // other server-side action calls.
+        // $A.enqueueAction adds the server-side action to the queue.
+        $A.enqueueAction(action);
+    },
+    /**
      * Fire toast event
      * @param {*} component
      * @param {*} title
      * @param {*} message
      * @param {*} type success/error/info
      */
-    showToast : function(component, title, message, type) {
-        var toastEvent = $A.get("e.force:showToast");
-        toastEvent.setParams({
+    showToast : function(component, title, message, type, mode) {
+        component.find('notifLib').showToast({
             "title": title,
-            "mode": (type == 'error' ? 'sticky' : 'dismissible'),
-            "message": message,
-            "type": (type == null ? 'info' : type)
+            "message":message,
+            "variant": (type == null ? 'info' : type),
+            "mode": (mode == null ? ((type == 'info' || type == 'success' || type == null) ? 'dismissable' : 'sticky') : mode)
         });
-        toastEvent.fire();
     },
     /**
      * Starts the flow with the attribute name 'flowName'
