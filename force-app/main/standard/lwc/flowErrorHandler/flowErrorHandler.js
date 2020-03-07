0e210521f43ff3d4221799fd560d231f9c0d1c8e
jaapbranderhorst <jaap.branderhorst@appsolutely.nl>
2020-03-07 21:51:35 +0100

    Forgot to add Flow_Settings__mdt.object-meta.xml

--- force-app/main/standard/lwc/flowErrorHandler/flowErrorHandler.js
+++ force-app/main/standard/lwc/flowErrorHandler/flowErrorHandler.js
@@ -11,10 +11,6 @@ export default class FlowErrorHandler extends LightningElement {
 
     @api title = 'Sample Title';
     @api message = 'Sample Message';
-    // @api variant = 'error';
-    // @api autoCloseTime = 5000;
-    // @api autoClose = false;
-    // @api autoCloseErrorWarning = false;
 
     toastRendered = false;
 
@@ -24,15 +20,5 @@ export default class FlowErrorHandler extends LightningElement {
             this.toastRendered = true;
         }
     }
-
-    // showToast() {
-    //     const event = new ShowToastEvent({
-    //         title: this.title,
-    //         message: this.errorMessage
-    //     });
-    //     this.dispatchEvent(event);
-    // }
-
-
 
 }
