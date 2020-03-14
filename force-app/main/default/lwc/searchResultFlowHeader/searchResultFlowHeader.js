24c8d7ab8f44e1abbd16d781f940e62608934700
jaapbranderhorst <jaap.branderhorst@appsolutely.nl>
2020-03-14 22:49:04 +0100

    Added specialized headers searchFlowHeader.html and searchResultFlowHeader.html for respectively a screen with search criteria and a screen with search results.

---
+++ force-app/main/default/lwc/searchResultFlowHeader/searchResultFlowHeader.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jaapbranderhorst on 14/03/2020.
+ */
+
+import {LightningElement} from 'lwc';
+
+import Search_Results_Title from '@salesforce/label/c.Search_Results_Title';
+
+export default class SearchResultFlowHeader extends LightningElement {
+
+    label = {
+        Search_Results_Title
+    }
+
+}
