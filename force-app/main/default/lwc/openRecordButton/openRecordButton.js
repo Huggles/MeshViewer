73558466f80eb09d83df14d0c46e09671f06294a
hugovankrimpen <HugoVK15@gmail.com>
2020-03-18 15:42:05 +0100

    Removed Open Record Button from header

--- force-app/main/default/lwc/openRecordButton/openRecordButton.js
+++ force-app/main/default/lwc/openRecordButton/openRecordButton.js
@@ -4,16 +4,14 @@
 
 import {LightningElement, api} from 'lwc';
 import {NavigationMixin} from 'lightning/navigation';
-import Open_Business_Dossier from '@salesforce/label/c.Open_Business_Dossier';
 
 export default class OpenRecordButton extends NavigationMixin(LightningElement) {
 
     @api
     recordId;
 
-    label = {
-        Open_Business_Dossier
-    }
+    @api
+    label;
 
     handleOnClick() {
         this[NavigationMixin.Navigate]({
