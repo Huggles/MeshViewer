79efa2004662d0492d97e7db975f50720706bd3e
jaapbranderhorst <jaap.branderhorst@appsolutely.nl>
2020-05-09 15:07:04 +0200

    Started implementing licenseTypeManagementCard.js. Added some methods to ILicenseManagementService.cls and its implementation to make this possible. Extended LicenseType.cls with some test values.

--- force-app/main/modules/setup/lwc/licenseTypeManagementCard/licenseTypeManagementCard.js
+++ force-app/main/modules/setup/lwc/licenseTypeManagementCard/licenseTypeManagementCard.js
@@ -7,22 +7,27 @@ import {api, LightningElement} from 'lwc';
 export default class LicenseTypeManagementCard extends LightningElement {
 
     /**
-     * Contains the name of the License Type (Company.info for Business for example).
+     * Contains the API name of the license type as defined in the LicenseType enum in Apex
      */
     @api
+    licenseTypeName;
+
+    /**
+     * Contains the name of the License Type (Company.info for Business for example).
+     */
     title;
 
     /**
      * Contains the API name of the feature parameter of type number containing the number of seats for this license type the customer is entitled to.
      */
-    @api
     nrOfSeatsFeatureParam;
 
     /**
      * Contains the API name of the permission group containing the permissions granted by having a license from this license type.
      */
-    @api
     permissionGroupName;
+
+
 
 
 }
