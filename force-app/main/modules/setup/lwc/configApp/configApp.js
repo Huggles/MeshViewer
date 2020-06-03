46640712c078eb006e0b3751bd35ffcbcb6acaf1
Hugo van Krimpen <hugo.van.krimpen@appsolutely.nl>
2020-06-03 15:02:37 +0200

    Added an extra label. Renamed a folder.

--- force-app/main/modules/setup/lwc/configApp/configApp.js
+++ force-app/main/modules/setup/lwc/configApp/configApp.js
@@ -5,66 +5,18 @@ import { LightningElement, track, wire } from 'lwc';
 // import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 // import Config_Account_AwaitingActivation from '@salesforce/label/c.Config_Account_AwaitingActivation';
 import Config_Title from '@salesforce/label/c.Config_Title';
+import Config_Credentials from '@salesforce/label/c.Config_Credentials';
+import Update_types from '@salesforce/label/c.Update_types';
+
+
+
 // import Config_Page_Title from '@salesforce/label/c.Config_Page_Title';
 // import Config_Loading from '@salesforce/label/c.Config_Loading';
 
 export default class configApp extends LightningElement {
-
-    // @track onboarded = false;
-    // @track ready = false;
-    // @track showOnboarded = false;
-    //
-    // isUserOnboarded;
-    // @track userActive;
-    //
-    // activeData;
-    //
-    label = {
-        //Config_Account_AwaitingActivation,
+    labels = {
         Config_Title,
-        // Config_Page_Title,
-        // Config_Loading
+        Config_Credentials,
+        Update_types
     };
-    //
-    // @wire(getUserOnboarded)
-    // wiredUserOnboarded(result){
-    //     this.isUserOnboarded = result;
-    //     if(result.error){
-    //         this.error = result.error;
-    //         this.users = undefined;
-    //     }else{
-    //
-    //         this.onboarded = result.data;
-    //         this.ready = true;
-    //         this.error = undefined;
-    //         if(this.onboarded === false){
-    //             this.showOnboarded = true; // to hide onboarding button during load
-    //         }
-    //     }
-    // }
-    //
-    // @wire(userCheckActive)
-    // wiredActive(result) {
-    //     this.activeData = result;
-    //     if (result.data !== undefined) {
-    //         this.userActive = result.data;
-    //         this.error = undefined;
-    //     } else if (result.error) {
-    //         this.error = result.error;
-    //         this.users = undefined;
-    //     }
-    // }
-    //
-    // handleUserOnboarded(){
-    //     this.onboarded = true;
-    //     refreshApex(this.activeData);
-    // }
-    //
-    // get canShowOnboarded() {
-    //     return this.showOnboarded || this.userActive;
-    // }
-    //
-    // get awaitingActivation() {
-    //     return !this.userActive && this.onboarded;
-    // }
 }
