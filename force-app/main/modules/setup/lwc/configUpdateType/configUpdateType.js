9b90ca25d37d4fda40c5ba127a311c923d1852b7
Hugo van Krimpen <hugo.van.krimpen@appsolutely.nl>
2020-06-03 12:39:28 +0200

    Updated description. Fixed the Dual List Box connection with APEX

--- force-app/main/modules/setup/lwc/configUpdateType/configUpdateType.js
+++ force-app/main/modules/setup/lwc/configUpdateType/configUpdateType.js
@@ -5,6 +5,9 @@
 import { LightningElement, wire, track } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 
+import Error from '@salesforce/label/c.Error';
+import Success from '@salesforce/label/c.Success';
+
 import getUpdateSettings from '@salesforce/apex/UpdateSettingSelector.getUpdateSettings';
 import setUpdateSettings from '@salesforce/apex/UpdateSettingSelector.setUpdateSettings';
 
@@ -12,31 +15,74 @@ export default class ConfigUpdateType extends LightningElement {
     @track options = [];
     @track values = [];
 
-    connectedCallback() {
-        this.retrieveUpdateSettings();
+    isLoading = false;
 
+    connectedCallback() {
+        this.isLoading = true;
+        this.retrieveUpdateSettings();
     }
 
+    updateSettingWrappers = [];
     retrieveUpdateSettings(){
         getUpdateSettings({})
             .then(result => {
-                let items = [];
-                for (const property in result) {
-                    console.log(property);
-                    if(property.includes('appsolutely__')){
-                        console.log('pushing!');
-                        this.options.push({
-                            label: property,
-                            value: property
-                        });
-                    }
-                }
+                this.updateSettingWrappers = result;
+                this.setupDualListBox(result);
             })
             .catch(error => {
                 console.log(error);
             })
     }
-    setupDualListBox(){
+    setupDualListBox(updateSettingWrappers){
+        console.log(updateSettingWrappers);
+        updateSettingWrappers.forEach((wrapper, index) => {
+            this.options.push({
+                label: wrapper.label,
+                value: wrapper.apiName
+            });
+            if(wrapper.value == true){
+                this.values.push(wrapper.apiName);
+            }
+            this.isLoading = false;
+        });
+    }
+    handleUpdateSettingChange(event){
+        this.isLoading = true;
+        let selectedValues = event.detail.value;
+
+        //First get all wrapper objects based on what is selected
+        let trueValues = this.updateSettingWrappers.filter(function(wrapper) {
+            return selectedValues.includes(wrapper.apiName);
+        });
+        let falseValues = this.updateSettingWrappers.filter(function(wrapper) {
+            return !selectedValues.includes(wrapper.apiName);
+        });
+
+        //Then set all wrapper object values based on what is selected
+        trueValues.forEach((wrapper, index) => {
+            wrapper.value = true;
+        });
+        falseValues.forEach((wrapper, index) => {
+            wrapper.value = false;
+        });
+
+        //Combine both arrays
+        let allUpdatedValues = trueValues.concat(falseValues);
+
+        //Pass it to the server
+        setUpdateSettings({ wrappers : allUpdatedValues })
+            .then(result => {
+                this.isLoading = false;
+            })
+            .catch(error => {
+                const event = new ShowToastEvent({
+                    title: Error,
+                    message: error,
+                    variant: 'error'
+                });
+                this.dispatchEvent(event);
+                this.isLoading = false;
+            })
 
     }
 
