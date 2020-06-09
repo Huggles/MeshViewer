86262d31f8bea29143f62a8a42917fa1ee11e5b2
Hugo van Krimpen <hugo.van.krimpen@appsolutely.nl>
2020-06-09 10:53:28 +0200

    Fixed double sfdx statement

--- scripts/operations/assign-permission-set.sh
+++ scripts/operations/assign-permission-set.sh
@@ -9,4 +9,4 @@ SCRATCH_ORG_ALIAS=$1
 PERMISSION_SET_NAME=$2
 
 echo "RUNNING: sfdx force:user:permset:assign -u \"$SCRATCH_ORG_ALIAS\" -n \"$PERMISSION_SET_NAME\""
-sfdx sfdx force:user:permset:assign -u "$SCRATCH_ORG_ALIAS" -n "$PERMISSION_SET_NAME"
+sfdx force:user:permset:assign -u "$SCRATCH_ORG_ALIAS" -n "$PERMISSION_SET_NAME"
