b6f6e2772c0f0474df87dafd1d8a7a5eb63c22a6
Hugo van Krimpen <hugo.van.krimpen@appsolutely.nl>
2020-06-11 15:28:02 +0200

    Commit scripts fi

---
+++ scripts/operations/enable-lc-debug-mode.sh
@@ -0,0 +1,10 @@
+########################################################################################################
+# Author      : Hugo van Krimpen
+# Date        : 08-06-2020
+# Parameter 1 : Alias/Username of the scratch org / user
+########################################################################################################
+
+SCRATCH_ORG_ALIAS=$1
+
+
+echo "update new User(Id = UserInfo.getUserId(), UserPreferencesUserDebugModePref=true);" | sfdx force:apex:execute -u "$SCRATCH_ORG_ALIAS"
---
+++ scripts/operations/install-package-version.sh
@@ -0,0 +1,12 @@
+########################################################################################################
+# Author      : Hugo van Krimpen
+# Date        : 08-06-2020
+# Parameter 1 : Alias/Username of the scratch org / user
+# Parameter 2 : 04t/Alias of the package version
+########################################################################################################
+
+SCRATCH_ORG_ALIAS=$1
+PACKAGE_VERSION_NAME=$2
+
+echo "RUNNING: sfdx force:package:install -u \"$SCRATCH_ORG_ALIAS\" -p \"$PACKAGE_VERSION_NAME\" -w 15"
+sfdx force:package:install -u "$SCRATCH_ORG_ALIAS" -p "$PACKAGE_VERSION_NAME" -w 15 -r
