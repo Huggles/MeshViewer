fe0bab7683569a708bb0636c055f0f6c3808a590
Hugo van Krimpen <hugo.van.krimpen@appsolutely.nl>
2020-08-27 14:20:11 +0200

    Overhauled the provinces location search

--- force-app/main/modules/findBusinesses/lwc/findBusinessesLocation/findBusinessesLocation.js
+++ force-app/main/modules/findBusinesses/lwc/findBusinessesLocation/findBusinessesLocation.js
@@ -13,140 +13,99 @@ import Remove_All from '@salesforce/label/c.Remove_All'
 
 export default class FindBusinessesLocation extends LightningElement {
 
-    MAP_LAYERS = {
-        PROVINCES : 'PROVINCES',
-        POSTALCODE : 'POSTALCODE'
-    }
-
+    /*
+     * The labels used in this screen
+     */
     labels = {
         Select_All,
         Remove_All
     }
 
     /*
-    * The mutable province data determining how the features should be shown on the map.
-    */
+     * All map layers
+     */
+    MAP_LAYERS = {
+        PROVINCES : 'PROVINCES',
+        POSTALCODE : 'POSTALCODE'
+    }
 
-    @track _provinces = {
-        'Drenthe': {
-            show_on_map: false
-        },
-        'Flevoland': {
-            show_on_map: false
-        },
-        'Friesland': {
-            show_on_map: false
-        },
-        'Gelderland': {
-            show_on_map: false
-        },
-        'Groningen': {
-            show_on_map: false
-        },
-        'Limburg': {
-            show_on_map: false
-        },
-        'Noord-Brabant': {
-            show_on_map: false
-        },
-        'Noord-Holland': {
-            show_on_map: false
-        },
-        'Overijssel': {
-            show_on_map: false
-        },
-        'Utrecht': {
-            show_on_map: false
-        },
-        'Zeeland': {
-            show_on_map: false
-        },
-        'Zuid-Holland': {
-            show_on_map: false
-        }
+    /*
+     * Styles to present the map
+     */
+    selectedStyle = {
+        opacity : 0.5,
+        color : '#a09f9f',
+        fillColor : 'green',
+        fillOpacity : 0.8,
+        radius: 300,
+        weight : 1,
+    }
+    deselectedStyle = {
+        opacity : 0.2,
+        color : '#a09f9f',
+        fillColor : 'red',
+        fillOpacity : 0.5,
+        radius: 250,
+        weight : 1,
+    }
+
+    /*
+     * Sets the selected layer to present.
+     */
+    _selectedMapLayer = this.MAP_LAYERS.POSTALCODE;
+    get selectedMapLayer(){
+        return this._selectedMapLayer;
+    }
+    set selectedMapLayer(value){
+        this._selectedMapLayer = value;
     }
 
     @api selectedProvinces = {}
 
     @api getLocationArray(){
         let selectedProvinces = [];
-        for (const [key, value] of Object.entries(this._provinces)) {
-            if(value.show_on_map === true) selectedProvinces.push(key);
+        for (const [key, value] of Object.entries(this.provinces)) {
+            if(value.selected === true) selectedProvinces.push(key);
         }
         return selectedProvinces;
     }
 
-
-
-
     /*
-     * Turn the map into an array so it can be shown as a list in HTML.
+     * Leaflet Properties
      */
-    get provincesArray(){
-        let returnArray = [];
-        for(const [key, value] of Object.entries(this._provinces)){
-            returnArray.push({
-                name : key,
-                show_on_map : value.show_on_map
-            })
-        }
-        return returnArray;
-    }
-
-    selectedStyle = {
-        opacity : 1,
-        color : 'green',
-        fillColor : 'green',
-        fillOpacity : 1,
-        radius: 300,
-        weight : 1,
-    }
-    deselectedStyle = {
-        opacity : 0.2,
-        color : 'red',
-        fillColor : 'red',
-        fillOpacity : 0.5,
-        radius: 250,
-        weight : 1,
-    }
-    get selectedPostalCodeStyle(){
-        let selectedStyle = JSON.parse(JSON.stringify(this.selectedStyle));
-        //selectedStyle['radius'] = 500;
-        //console.log(selectedStyle);
-        return selectedStyle;
-    }
-    get deselectedPostalCodeStyle(){
-        let deselectedStyle = JSON.parse(JSON.stringify(this.deselectedStyle));
-        //deselectedStyle['radius'] = 500;
-        //console.log(deselectedStyle);
-        return deselectedStyle;
-    }
-
-
     _leafletMap;
     _leafletMapHTMLElement;
     _leafletLibraryLoaded = false;
     _leafletMapLoaded = false;
-
     _openStreetMapTileLayer;
 
-    _geoJSONProvinces;
-    _geoJSONProvincesLayer;
-
-    postalCodesData;
-    @track postalCodes = {};
+    loadingMap = false;
 
     connectedCallback() {
+        this.loadingMap = true;
         this.loadInitialData();
         this.loadLeaflet();
     }
     loadInitialData(){
         if(this.selectedProvinces != null && Object.entries(this.selectedProvinces).length > 0){
             this.selectedProvinces.forEach((selectedProvince)=>{
-                this._provinces[selectedProvince].show_on_map = true;
+                this.provinces[selectedProvince].show_on_map = true;
             });
         }
     }
+    renderedCallback() {
+        this._leafletMapHTMLElement = this.template.querySelector("[data-identifier='leafletMap']");
+        if(this._leafletMapHTMLElement  != null){
+            this.initLeafletMap();
+        }
+    }
+
+
+
+
+    /*
+     * General Functions
+     */
     loadLeaflet(){
         Promise.all([
             loadScript(this, leaflet + '/leaflet.js'),
@@ -166,7 +125,7 @@ export default class FindBusinessesLocation extends LightningElement {
         let request = new XMLHttpRequest();
         request.open("GET", GeoJSONProvinces, false);
         request.send(null);
-        this._geoJSONProvinces = JSON.parse(request.responseText)[0];
+        this.provincesData = JSON.parse(request.responseText)[0];
     }
     loadGeoJSONPostalCodes(){
         let request = new XMLHttpRequest();
@@ -174,22 +133,13 @@ export default class FindBusinessesLocation extends LightningElement {
         request.send(null);
         this.postalCodesData = JSON.parse(request.responseText);
     }
-
-    renderedCallback() {
-        this._leafletMapHTMLElement = this.template.querySelector("[data-identifier='leafletMap']");
-        if(this._leafletMapHTMLElement  != null){
-            this.initLeafletMap();
-        }
-
-    }
-
     initLeafletMap(){
         if(this._leafletMapHTMLElement != null && this._leafletLibraryLoaded == true && this._leafletMapLoaded == false){
             this._leafletMapLoaded = true;
             this._leafletMap = L.map(this._leafletMapHTMLElement, {
                 zoomDelta: 0.25,
                 zoomSnap: 0.25,
-
+                preferCanvas: true,
             }).setView([52.2044968, 5.6275306], 6.5);
             this._openStreetMapTileLayer = L.tileLayer(
                 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -199,120 +149,139 @@ export default class FindBusinessesLocation extends LightningElement {
                 })
                 .addTo(this._leafletMap);
             try{
-                this.initProvincesGeoJSON();
+                this.initProvinces();
                 this.initPostalCodes();
 
             }catch (e) {
                 console.log(e);
             }
-            //this.showProvinces();
-            this.showAllPostalCodes();
-
-
+            this.loadingMap = false;
         }
     }
 
-    initProvincesGeoJSON(){
-        if(this._geoJSONProvincesLayer == null){
-            this._geoJSONProvincesLayer = L.geoJSON(this._geoJSONProvinces,{
-                onEachFeature: ((feature, layer) =>{
-                    layer.setStyle(this._provinces[feature.properties.name].show_on_map ? this.selectedStyle : this.deselectedStyle);
-                    layer.on('click', (event)=>{
-                        this.provinceFeatureClicked(feature,layer,this);
-                    });
-                })
-            });
+    /*
+    * Handle the click on a tab
+    */
+    handleTabClick(event){
+        let tabValue = event.target.value;
+        this.selectedMapLayer = tabValue;
+        if(this._leafletMap != null){
+            this.showLayer(tabValue);
         }
-    }
-    initPostalCodes(){
-        let controller = this;
-        this.postalCodesData.forEach((item, index)=>{
-            let postalCodeCircle = L.circle([item.Lat, item.Lon], this.deselectedPostalCodeStyle);
-            postalCodeCircle.on("click", (feature, layer)=>{
-                console.log('clicked');
-                let postalCode = item.Code.toString();
-                console.log(postalCode);
-                console.log(controller.postalCodes[postalCode].selected);
-                if(!controller.postalCodes[postalCode].selected){
-                    controller.selectPostalCode(postalCode);
-                }else{
-                    controller.deselectPostalCode(postalCode);
-                }
-            });
-            item['layerElement'] = postalCodeCircle;
-            item['selected'] = false;
-            item['label'] = item.Code + ' - ' + item.City;
-            this.postalCodes[item.Code] = item;
-        });
-
     }
     showLayer(layerName){
-        if(layerName === this.MAP_LAYERS.PROVINCES) this.showProvinces();
-        if(layerName === this.MAP_LAYERS.POSTALCODE) this.showAllPostalCodes();
-    }
-    showProvinces(){
-        this.hideAllPostalCodes();
-        this._geoJSONProvincesLayer.addTo(this._leafletMap);
-        this._leafletMap.setMaxBounds(this._geoJSONProvincesLayer.getBounds());
-    }
-    showAllPostalCodes(){
-        this._leafletMap.removeLayer(this._geoJSONProvincesLayer);
-        for (const [key, value] of Object.entries(this.postalCodes)) {
-            if(value.selected){
-                value.layerElement.setStyle(this.selectedPostalCodeStyle);
-            }else{
-                value.layerElement.setStyle(this.deselectedPostalCodeStyle);
-            }
-            value.layerElement.addTo(this._leafletMap);
+        if (layerName == this.MAP_LAYERS.PROVINCES){
+            this._leafletMap.removeLayer(this.postalCodesLayer);
+            this.provincesLayer.addTo(this._leafletMap);
+        }
+        if (layerName == this.MAP_LAYERS.POSTALCODE){
+            this._leafletMap.removeLayer(this.provincesLayer);
+            this.postalCodesLayer.addTo(this._leafletMap);
         }
     }
-    hideAllPostalCodes(){
-        for (const [key, value] of Object.entries(this._provinces)) {
-            if(value.selected) {
-                this._leafletMap.removeLayer(value.layerElement);
-            }
-        }
-    }
-    getProvinceFeatureLayer(provinceName){
-        let foundLayer = null;
-        this._geoJSONProvincesLayer.eachLayer((layer) => {
-            if (layer.feature.properties.name == provinceName) {
-                foundLayer = layer;
-            }
-        });
-        return foundLayer;
-    }
-    provinceFeatureClicked(feature, layer, controller){
-        let show_on_map = controller._provinces[feature.properties.name].show_on_map;
-        try{
-            if(!show_on_map){
-                controller.showFeature(feature, layer, controller);
-            }else{
-                controller.hideFeature(feature, layer, controller)
-            }
-        }catch (e){
-            console.log(e);
-        }
-    }
-    postalCodeFeatureClicked(feature, layer, controller){
 
-    }
-    showFeature(feature, layer, controller){
-        controller._provinces[feature.properties.name].show_on_map = true;
-        layer.setStyle(this.selectedStyle);
-    }
-    hideFeature(feature, layer, controller){
-        controller._provinces[feature.properties.name].show_on_map = false;
-        layer.setStyle(this.deselectedStyle);
+
+    /*
+     * Province Functions
+     */
+
+    /*
+     * Province data
+     */
+    provincesData;
+    provincesLayer;
+    @track provinces = {};
+
+    initProvinces(){
+        if(this.provincesLayer == null){
+            let payload = {
+                onEachFeature: ((feature, layer) =>{
+                    let featureData = {};
+                    featureData['properties'] = feature.properties;
+                    featureData['layerElement'] = layer;
+                    featureData['id'] = feature.properties.name;
+                    featureData['name'] = feature.properties.name;
+                    featureData['selected'] = false;
+                    layer.setStyle(this.deselectedStyle);
+                    layer.on('click', (event)=>{
+                        this.provinceFeatureClicked(featureData.id,this);
+                    });
+                    this.provinces[feature.properties.name] = featureData;
+                })
+            };
+            this.provincesLayer = L.geoJSON(this.provincesData,payload);
+        }
     }
 
     provinceSelectionChanged(event) {
-        try{
-            let provinceLayer = this.getProvinceFeatureLayer(event.target.name);
-            this.provinceFeatureClicked(provinceLayer.feature, provinceLayer, this);
+        let provinceId = event.currentTarget.dataset['id'];
+        this.provinceFeatureClicked(provinceId, this);
+    }
+    provinceFeatureClicked(province_id, controller){
+        let provinceData = controller.provinces[province_id];
+        let selected = provinceData.selected;
+        try {
+            if(!selected){
+                controller.provinces[province_id].selected = true;
+                provinceData.layerElement.setStyle(this.selectedStyle);
+            }else{
+                controller.provinces[province_id].selected = false;
+                provinceData.layerElement.setStyle(this.deselectedStyle);
+            }
         }catch (e){
             console.log(e);
         }
+
+    }
+
+    /*
+   * Turn the map into an array so it can be shown as a list in HTML.
+   */
+    get provincesArray(){
+        let returnArray = [];
+        for(const [key, value] of Object.entries(this.provinces)){
+            returnArray.push(value);
+        }
+        return returnArray;
+    }
+
+    /*
+     * Postal Code Functions
+     */
+
+    /*
+     * Postal Code
+     */
+    postalCodesData;
+    postalCodesLayer;
+    postalCodesDataLoaded = false;
+    @track postalCodes = {};
+
+    initPostalCodes(){
+        if(this.postalCodesDataLoaded == false){
+            this.postalCodesDataLoaded = true;
+            this.postalCodesLayer = L.layerGroup().addTo(this._leafletMap);
+            let controller = this;
+            this.postalCodesData.forEach((item, index)=>{
+                let postalCodeCircle = L.circle([item.Lat, item.Lon], this.deselectedStyle);
+                postalCodeCircle.on("click", (feature, layer)=>{
+                    let postalCodeId = item.id;
+                    if(!controller.postalCodes[postalCodeId].selected){
+                        controller.selectPostalCode(postalCodeId);
+                    }else{
+                        controller.deselectPostalCode(postalCodeId);
+                    }
+                });
+                item['layerElement'] = postalCodeCircle;
+                item['selected'] = false;
+                item['label'] = item.Code + ' - ' + item.City;
+                item['id'] = item.Code + '-' + item.City;
+                this.postalCodes[item.id] = item;
+                this.postalCodesLayer.addLayer(postalCodeCircle);
+            });
+        }
+
+
     }
 
     @track postalCodeSearchMatches = [];
@@ -332,38 +301,36 @@ export default class FindBusinessesLocation extends LightningElement {
         this.postalCodeSearchMatches = matchArray;
     }
 
-
     onPostalCodeSearchMatchClick(event){
-        let postalCode = event.currentTarget.dataset['postalcode'].toString();
+        let postalCode = event.currentTarget.dataset['id'].toString();
         this.selectPostalCode(postalCode);
     }
-    selectPostalCode(postalCode){
-        this.postalCodes[postalCode].selected = true;
-        this.postalCodes[postalCode].layerElement.setStyle(this.selectedPostalCodeStyle);
-        this.postalCodes[postalCode].layerElement.setRadius(this.selectedPostalCodeStyle.radius);
+    selectPostalCode(id){
+        this.postalCodes[id].selected = true;
+        this.postalCodes[id].layerElement.setStyle(this.selectedStyle);
+        this.postalCodes[id].layerElement.setRadius(this.selectedStyle.radius);
         this.findMatchingPostalCodes();
     }
-    deselectPostalCode(postalCode){
-        console.log('deselectPostalCode');
-        this.postalCodes[postalCode].selected = false;
-        this.postalCodes[postalCode].layerElement.setStyle(this.deselectedPostalCodeStyle);
-        this.postalCodes[postalCode].layerElement.setRadius(this.deselectedPostalCodeStyle.radius);
+    deselectPostalCode(id){
+        this.postalCodes[id].selected = false;
+        this.postalCodes[id].layerElement.setStyle(this.deselectedStyle);
+        this.postalCodes[id].layerElement.setRadius(this.deselectedStyle.radius);
         this.findMatchingPostalCodes();
     }
     onSelectedPostalCodeRemove(event){
-        let postalCode = event.currentTarget.dataset['postalcode'].toString();
-        this.deselectPostalCode(postalCode);
+        let id = event.currentTarget.dataset['id'];
+        this.deselectPostalCode(id);
     }
     onPostalCodeSelectAllClick(event){
         this.postalCodeSearchMatches.forEach((item, index)=>{
-            this.selectPostalCode(item.Code.toString());
+            this.selectPostalCode(item.id);
         });
         this.findMatchingPostalCodes();
     }
     onRemoveAllSelectedPostalCodes(event){
         for (const [key, value] of Object.entries(this.postalCodes)) {
             if(value.selected){
-                this.deselectPostalCode(value.Code);
+                this.deselectPostalCode(value.id);
             }
         }
     }
@@ -379,14 +346,11 @@ export default class FindBusinessesLocation extends LightningElement {
 
     _searchInputHasFocus = false;
     onSearchInputFocus(event){
-        console.log('onSearchInputFocus');
         this._searchInputHasFocus = true;
     }
     onSearchInputBlur(event){
-        console.log('onSearchInputBlur');
         this._searchInputHasFocus = false;
     }
-
     get showMatches(){
         if(this.postalCodeSearchMatches != null && this.postalCodeSearchMatches.length > 0 && this._searchInputHasFocus){
             return true;
