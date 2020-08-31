1a575e653db48e2a422589d3df18f163ad8d6edb
Hugo van Krimpen <hugo.van.krimpen@appsolutely.nl>
2020-08-31 15:18:54 +0200

    Made salesforceIcon.html a seperate component.

---
+++ force-app/main/default/lwc/salesforceIcon/salesforceIcon.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Hugo on 31/08/2020.
+ */
+
+import {LightningElement,api} from 'lwc';
+
+export default class SalesforceIcon extends LightningElement {
+
+    @api iconSize = 'x-small'
+}
